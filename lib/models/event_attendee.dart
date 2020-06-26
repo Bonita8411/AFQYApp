2b0d0226a7c404ae7031c5adb68db0bf4ae21d87
Liam McAteer <liam@liammcateer.nz>
2020-06-27 10:25:13 +1200

    removed redundant connection information from attendee model

--- lib/models/event_attendee.dart
+++ lib/models/event_attendee.dart
@@ -4,11 +4,6 @@ class EventAttendee{
   List<String> interests = [];
   bool verified;
   bool saved;
-  List<EventAttendee> connections = [];
 
-  EventAttendee({this.name, this.ticketID, this.interests, this.verified = false, this.saved = false, this.connections});
-
-  Future addConnection(String ticketID) async{
-
-  }
+  EventAttendee({this.name, this.ticketID, this.interests, this.verified = false, this.saved = false});
 }
