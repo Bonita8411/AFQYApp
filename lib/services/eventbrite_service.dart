f2e69085c144ed8a44b5690bdf0cf6d6c36cd965
Liam McAteer <liam@liammcateer.nz>
2020-06-26 15:33:42 +1200

    added production switch to eventbrite service

--- lib/services/eventbrite_service.dart
+++ lib/services/eventbrite_service.dart
@@ -3,7 +3,9 @@ import 'package:afqyapp/models/Eventbrite_event.dart';
 import 'package:http/http.dart' as http;
 
 class EventbriteService{
-  static String _apiKey = "OYFDYJB7SMZ2VFQG5CO6";
+  static bool isProduction = false;
+  static String _apiKey = isProduction ? "OYFDYJB7SMZ2VFQG5CO6" : "ZGD5HQHQPTGYG3F5S2YI";
+  static String _organizationId = isProduction ? "47604957273" : "429473716086";
   static List<EventbriteEvent> _loadedEvents = [];
 
   static Future<List<EventbriteEvent>> getEvents() async {
@@ -15,7 +17,8 @@ class EventbriteService{
 
   static Future<List<EventbriteEvent>> refreshEvents() async{
     try {
-      String eventListUrl = "https://www.eventbriteapi.com/v3/organizations/47604957273/events/?expand=venue&order_by=start_desc&status=live&token=" + _apiKey;
+      String eventListUrl = "https://www.eventbriteapi.com/v3/organizations/${_organizationId}/events/?expand=venue&order_by=start_desc&status=live&token=" + _apiKey;
+      print(eventListUrl);
       http.Response response = await http.get(eventListUrl);
       List events = json.decode(response.body)['events'];
 
