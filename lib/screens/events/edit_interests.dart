64c4afbccfbb2d19afd1c53cb3c3fe1591f0a1ee
Liam McAteer <liam@liammcateer.nz>
2020-07-02 17:16:22 +1200

    update and retrieve interests
    added functionality to update and retrieve user's interests for an event.
    also fixed refresh indicator bug where indicator finished imediately.

--- lib/screens/events/edit_interests.dart
+++ lib/screens/events/edit_interests.dart
@@ -9,66 +9,27 @@ class EditInterestsScreen extends StatefulWidget {
   _EditInterestsScreenState createState() => _EditInterestsScreenState();
 }
 
-class Interest {
-  String interest;
-
-  Interest(this.interest);
-
-  static List<Interest> getInterests(){
-    return <Interest>[
-      Interest('Cooking'),
-      Interest('Baking'),
-      Interest('Eating'),
-    ];
-  }
-}
-
 class _EditInterestsScreenState extends State<EditInterestsScreen> {
+  static List<String> interests = [
+    '',//Empty String for if people don't want to choose an interest
+    'Cooking',
+    'Baking',
+    'Eating',
+  ];
 
-  List<Interest> interest = Interest.getInterests();
-  List<DropdownMenuItem<Interest>> dropdownMenuItems;
-  Interest selectedInterest;
-  Interest selectedInterest2;
-  Interest selectedInterest3;
+  List<DropdownMenuItem<String>> dropdownMenuItems = interests.map((interest) {
+    return DropdownMenuItem<String>(
+      child: Text(interest),
+      value: interest,
+    );
+  }).toList();
+
+  Future<List> selectedInterestsFuture;
 
   @override
   void initState(){
-    dropdownMenuItems = buildDropdownMenuItems(interest);
-    selectedInterest = dropdownMenuItems[0].value;
-    selectedInterest2 = dropdownMenuItems[0].value;
-    selectedInterest3 = dropdownMenuItems[0].value;
     super.initState();
-  }
-
-  List<DropdownMenuItem<Interest>> buildDropdownMenuItems(List interest){
-    List<DropdownMenuItem<Interest>> items = List();
-    for (Interest int in interest){
-      items.add(
-        DropdownMenuItem(
-          value: int,
-          child: Text(int.interest),
-        ),
-      );
-    }
-    return items;
-  }
-
-  onChangeDropdownItem1(Interest selected){
-    setState(() {
-      selectedInterest = selected;
-    });
-  }
-
-  onChangeDropdownItem2(Interest selected){
-    setState(() {
-      selectedInterest2 = selected;
-    });
-  }
-
-  onChangeDropdownItem3(Interest selected){
-    setState(() {
-      selectedInterest3 = selected;
-    });
+    selectedInterestsFuture = widget.event.retrieveInterests();
   }
 
   Widget build(BuildContext context) {
@@ -77,38 +38,66 @@ class _EditInterestsScreenState extends State<EditInterestsScreen> {
         title: Text("Edit Interests"),
         backgroundColor: Colors.red[900],
       ),
-      body: new Container(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Text("Select an interest that best describes you"),
-              SizedBox(height: 20.0,),
-              DropdownButton(
-                value: selectedInterest,
-                items: dropdownMenuItems,
-                onChanged: onChangeDropdownItem1,
+      body: FutureBuilder(
+        future: selectedInterestsFuture,
+        builder: (context, snapshot){
+          if(snapshot.hasData){
+            List selectedInterests = snapshot.data;
+            return Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Please choose up to three interests that you would like to display.'),
+                  SizedBox(height: 20.0),
+                  DropdownButton(
+                    value: selectedInterests[0],
+                    items: dropdownMenuItems,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedInterests[0] = value;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 20.0),
+                  DropdownButton(
+                    value: selectedInterests[1],
+                    items: dropdownMenuItems,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedInterests[1] = value;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 20.0),
+                  DropdownButton(
+                    value: selectedInterests[2],
+                    items: dropdownMenuItems,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedInterests[2] = value;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 20.0),
+                  RaisedButton(
+                    child: Text('Update Interests'),
+                    onPressed: () {
+                      setState(() {
+                        selectedInterestsFuture = widget.event.updateInterests(selectedInterests);
+                      });
+                    },
+                  )
+                ],
               ),
-              SizedBox(height: 20.0,),
-              Text('Chosen: ${selectedInterest.interest}'),
-              SizedBox(height: 20.0,),
-              DropdownButton(
-                value: selectedInterest2,
-                items: dropdownMenuItems,
-                onChanged: onChangeDropdownItem2,
-              ),
-              SizedBox(height: 20.0,),
-              Text('Chosen: ${selectedInterest2.interest}'),
-              SizedBox(height: 20.0,),
-              DropdownButton(
-                value: selectedInterest3,
-                items: dropdownMenuItems,
-                onChanged: onChangeDropdownItem3,
-              ),
-              SizedBox(height: 20.0,),
-              Text('Chosen: ${selectedInterest3.interest}'),
-            ],
-          )
-      )
+            );
+          }else if(snapshot.hasError){
+            print(snapshot.error);
+            return Center(child: Text('Error'));
+          }else{
+            return Center(child: CircularProgressIndicator());
+          }
+        },
       )
     );
   }
