3c5ca775213e26b41088f190af017eabfbdf9f61
Liam McAteer <liam.mcateer@outlook.co.nz>
2020-07-29 13:53:41 +1200

    Revert "Feature/optimise retrieve attendees"

--- lib/models/event_attendee.dart
+++ lib/models/event_attendee.dart
@@ -1,4 +1,4 @@
-class EventAttendee with Comparable<EventAttendee>{
+class EventAttendee{
   String name;
   String ticketID;
   List<String> interests = [];
@@ -6,9 +6,4 @@ class EventAttendee with Comparable<EventAttendee>{
   bool saved;
 
   EventAttendee({this.name, this.ticketID, this.interests, this.verified = false, this.saved = false});
-
-  @override
-  int compareTo(EventAttendee other) {
-    return this.name.compareTo(other.name);
-  }
 }
