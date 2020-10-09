46a36b281c17b44661d4083dfea5c5db7d9d895c
Liam McAteer <liam@liammcateer.nz>
2020-10-10 11:54:37 +1300

    added lazy loading of old messages

--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -3,10 +3,14 @@ import 'package:firebase_database/firebase_database.dart';
 class MessageModel{
   String senderID;
   String message;
+  int timestamp;
   DateTime time;
+  String messageID;
 
   MessageModel.fromSnapshot(DataSnapshot snapshot):
     senderID = snapshot.value['s'],
     message = snapshot.value['m'],
-    time = DateTime.fromMillisecondsSinceEpoch(snapshot.value['t']).toLocal();
+    timestamp = snapshot.value['t'],
+    time = DateTime.fromMillisecondsSinceEpoch(snapshot.value['t']).toLocal(),
+    messageID = snapshot.key;
 }
--- lib/screens/chat/thread_screen.dart
+++ lib/screens/chat/thread_screen.dart
@@ -4,6 +4,7 @@ import 'package:afqyapp/models/user_profile.dart';
 import 'package:afqyapp/screens/chat/add_user_screen.dart';
 import 'package:afqyapp/services/message_service.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ThreadScreen extends StatefulWidget {
   final ThreadModel thread;
@@ -18,6 +19,9 @@ class _ThreadScreenState extends State<ThreadScreen> {
   String messageInput = '';
   final _formKey = GlobalKey<FormState>();
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  var _tapPosition;
+  ScrollController _controller;
+  bool _loadingMoreMessages = false;
 
   void _openEndDrawer() {
     _scaffoldKey.currentState.openEndDrawer();
@@ -25,6 +29,8 @@ class _ThreadScreenState extends State<ThreadScreen> {
 
   @override
   void initState() {
+    _controller = ScrollController();
+    _controller.addListener(_scrollListener);
     widget.thread.threadStateListener = () => setState(() {});
     widget.thread.updateReadTimestamp();
     super.initState();
@@ -34,6 +40,20 @@ class _ThreadScreenState extends State<ThreadScreen> {
   void dispose() {
     widget.thread.threadStateListener = () => {};
     super.dispose();
+  }
+
+  void _scrollListener(){
+    if (_controller.offset >= _controller.position.maxScrollExtent &&
+        !_controller.position.outOfRange) {
+      setState(() {
+        _loadingMoreMessages = true;
+      });
+      widget.thread.loadMoreMessages().then((value) {
+        setState(() {
+          _loadingMoreMessages = false;
+        });
+      });
+    }
   }
 
   @override
@@ -119,7 +139,7 @@ class _ThreadScreenState extends State<ThreadScreen> {
                                   )
                                 ],
                               ),
-                              onPressed: () async {
+                              onPressed: MessageService.instance.currentUserId != thread.owner ? null : () async {
                                 await thread.removeUser(user.uid);
                                 setState(() {
 
@@ -130,6 +150,12 @@ class _ThreadScreenState extends State<ThreadScreen> {
                         );
                       },
                     )
+                  ),
+                  RaisedButton(
+                    child: Text('load more'),
+                    onPressed: (){
+                      thread.loadMoreMessages();
+                    },
                   ),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
@@ -165,39 +191,75 @@ class _ThreadScreenState extends State<ThreadScreen> {
               },
               child: Text('Add people to this chat'),
             ))
-                : ListView.builder(
+                : Stack(
+                  children: [
+                    ListView.builder(
+              controller: _controller,
               reverse: true,
               itemCount: thread.messages.length,
               itemBuilder: (context, index){
-                MessageModel message = thread.messages[index];
-                bool fromCurrent = MessageService.instance.currentUserId == message.senderID;
-                UserProfile user = thread.participants.singleWhere((element) => element.uid == message.senderID);
-                return Align(
-                  alignment: fromCurrent ? Alignment.centerRight : Alignment.centerLeft,
-                  child: FractionallySizedBox(
-                    widthFactor: 0.8,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(height: 5.0,),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 5.0),
-                          child: Text(user.name,
-                            style: TextStyle(color: Colors.grey[600]),
-                          ),
+                    MessageModel message = thread.messages[index];
+                    bool fromCurrent = MessageService.instance.currentUserId == message.senderID;
+                    UserProfile user = thread.participants.singleWhere((element) => element.uid == message.senderID);
+                    return Align(
+                      alignment: fromCurrent ? Alignment.centerRight : Alignment.centerLeft,
+                      child: FractionallySizedBox(
+                        widthFactor: 0.8,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 5.0,),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 5.0),
+                              child: Text(user.name + " - " + DateFormat('d MMM yy, h:mm a').format(message.time),
+                                style: TextStyle(color: Colors.grey[600]),
+                              ),
+                            ),
+                            Card(
+                              color: fromCurrent ? Colors.red : Colors.white,
+                              child: GestureDetector(
+                                onTapDown: (details) => _tapPosition = details.globalPosition,
+                                onLongPress: (){
+                                  final RenderBox overlay = Overlay.of(context).context.findRenderObject();
+                                  showMenu(
+                                    context: context,
+                                    position: RelativeRect.fromRect(
+                                        _tapPosition & const Size(40, 40), // smaller rect, the touch area
+                                        Offset.zero & overlay.size   // Bigger rect, the entire screen
+                                    ),
+                                    items: [
+                                      PopupMenuItem<MessageModel>(
+                                          child: const Text('Report Message'), value: message),
+                                    ],
+                                  ).then((value) {
+                                    _showReportDialog().then((reason) {
+                                      thread.reportMessage(message, reason)
+                                          .then((value) {
+                                        Scaffold.of(context).showSnackBar(SnackBar(
+                                            content: Text('Your report has been sent.')
+                                        ));
+                                      }).catchError((error) {
+                                        Scaffold.of(context).showSnackBar(SnackBar(
+                                          content: Text('Failed to send report.'),
+                                        ));
+                                      });
+                                    });
+                                  });
+                                },
+                                child: ListTile(
+                                  title: Text(message.message),
+                                ),
+                              )
+                            ),
+                          ],
                         ),
-                        Card(
-                          color: fromCurrent ? Colors.red : Colors.white,
-                          child: ListTile(
-                            title: Text(message.message),
-                          )
-                        ),
-                      ],
-                    ),
-                  ),
-                );
+                      ),
+                    );
               },
             ),
+                    _loadingMoreMessages ? Align(alignment: Alignment.topCenter, child: CircularProgressIndicator()) : Container()
+                  ],
+                ),
           ),
           Divider(
             thickness: 2.0,
@@ -264,6 +326,45 @@ class _ThreadScreenState extends State<ThreadScreen> {
               child: Text('Leave'),
               onPressed: () {
                 Navigator.of(context).pop(true);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<String> _showReportDialog() async {
+    String _reason;
+    return showDialog<String>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Report this conversation'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Reason',
+                    labelText: 'Reason',
+                  ),
+                  onChanged: (val) => _reason = val,
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text('Send Report'),
+              onPressed: () {
+                Navigator.of(context).pop(_reason);
               },
             ),
           ],
