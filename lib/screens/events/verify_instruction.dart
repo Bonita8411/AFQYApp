78a29fd3f43f5b006ab5977958b741e7605d4588
Liam McAteer <liam@liammcateer.nz>
2020-10-16 15:50:43 +1300

    fixed grammar on verify instructions

--- lib/screens/events/verify_instruction.dart
+++ lib/screens/events/verify_instruction.dart
@@ -9,12 +9,13 @@ class _VerifyInstruction extends State<VerifyInstruction> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: new Text("How to verify your ticket "),
-      content: new Text("\nStep 1: Go to Who's Going Tab \n\nStep 2: Click on Verify Ticket button \n\nStep 3: Scan QR Code then Verify your ticket \n\nStep 4: You can edit your interest after verify your ticket "),
+      title: new Text("Did you just purchase a ticket?"),
+      content: new Text(
+          "\nStep 1: Go to Who's Going Tab \n\nStep 2: Click on Verify Ticket button \n\nStep 3: Scan the QR code on your ticket \n\nStep 4: You can edit your interests after verifying your ticket "),
       actions: <Widget>[
-        new FlatButton (
+        new FlatButton(
           child: new Text("Close"),
-          onPressed: (){
+          onPressed: () {
             Navigator.of(context).pop();
           },
         )
